4f6ded30dfe5246221e29c3160467bfe8d067b38
naduniw1990 <naduni.warnakulasooriya@1billiontech.com>
2022-09-01 23:52:24 +0530

    initial commit

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ import email_validator
 from functools import wraps
 from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
